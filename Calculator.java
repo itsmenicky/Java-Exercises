169b2ff13bc4d2a2f567f9832e075f910d0fa49b
Nickolas Gabriel <116317424+itsmenicky@users.noreply.github.com>
2023-04-15 17:30:38 -0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,129 +1,85 @@
-// Greetings User!! Code by itsmenicky ;)
-
-
 import java.util.Scanner;
-public class Calculator {
-    public static void main(String[]args) {
-      Scanner scan = new Scanner(System.in);
+import java.io.IOException;
+public class calculator {
 
-	       System.out.println("What is your name?: ");
-	       String name = scan.next();
+    public static final String reset = "\u001B[0m";
+    public static final String blue = "\u001B[34m";
+    public static final String green = "\u001B[32m";
+    public static final String red = "\u001B[31m";
+    public static final String yellow = "\u001B[33m";
+    public static void apagartela() throws IOException, InterruptedException{
+        if (System.getProperty("os.name").contains("Windows")){
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+          }
+          else{
+              Runtime.getRuntime().exec("clear");
+          }
+    }
+    public static void main(String[] args) throws IOException, InterruptedException{
+        //Greetings User!! Code by Isolated Thinker ;)
+        Scanner scan = new Scanner(System.in);
+        String nome, operacao, resposta;
+        double num1=0, num2=0, resultado;
 
-	       try {
-	           Double.parseDouble(name);
-	           System.out.println("Heyy...no numbers, enter with your name next time...exiting... ");
-	           System.exit(0);
-	       }catch(NumberFormatException e) {
-	           System.out.println("Welcome " + name);
-	       }
+        System.out.println("Insira seu nome: ");
+        nome = scan.next(); 
+        apagartela();
+        System.out.println(blue + "---|Bem-vindo a minha calculadora em Java " + nome +"!! Por: istmenicky|---" + reset);
 
-	       while (true){
-	           String operation, answer;
-	           double num1, num2, calculation;
-	           
-	           if (System.getProperty("os.name").contains("Windows"))
-	               new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-	           else
-	               Runtime.getRuntime().exec("clear");
+        while (true){
+            apagartela();
+            System.out.println(green + "-->Oque iremos fazer agora " + nome + "? (multiplicacao, divisao, adicao, subtracao): " + reset);
+            operacao = scan.next();
+            String verificar = operacao.substring(operacao.length()-2);
+            if(verificar.equals("ao")){
+                apagartela();
+                System.out.println("Insira o primeiro numero: ");
+                num1 = scan.nextDouble();
+                System.out.println("Insira o segundo numero: ");
+                num2 = scan.nextDouble();
+            }else{
+                System.out.println("Operacao Invalida!!");
+                System.exit(0);
+            }
+            apagartela();
+    
+            switch(operacao){
+                
+                case "multiplicacao":
+                     resultado = num1 * num2;
+                     System.out.println(green + "O resultado dessa " + operacao +" é " + resultado + reset);
+                     Thread.sleep(2000);
+                     break;
+                case "divisao":
+                     resultado = num1 / num2;
+                     System.out.println(green + "O resultado dessa " + operacao +" é " + resultado + reset);
+                     Thread.sleep(2000);
+                     break;
+                case "adicao":
+                     resultado = num1 + num2;
+                     System.out.println(green + "O resultado dessa " + operacao +" é " + resultado + reset);
+                     Thread.sleep(2000);
+                     break;  
+                case "subtracao":
+                     resultado = num1 - num2;
+                     System.out.println(green + "O resultado dessa " + operacao +" é " + resultado + reset);
+                     Thread.sleep(2000);
+                     break;   
 
-	           System.out.println("--> What we will gonna do now " + name + " (multiplication, division, sum or substraction)?");
-	           operation = scan.next();
+            }
+             apagartela();
+             System.out.println(yellow + "Deseja realizar outra operação??(s/n): " + reset);
+             resposta = scan.next();
+             if(resposta.equals("s")){
+                 continue;
+             }else if(resposta.equals("n")){
+                 break;
+             }else{
+                 System.out.println(red + "Resposta inválida!!" + reset);
+             }  
 
-	           if(operation.equals("multiplication")){
-	               System.out.println("Enter with the first factor: ");
-	               num1 = scan.nextDouble();
-
-	               System.out.println("Enter with the second factor: ");
-	               num2 = scan.nextDouble();
-
-	               calculation = num1 * num2;
-	               System.out.println(calculation);
-
-	               System.out.println("--> Want to do another operation? (y/n): ");
-	               answer = scan.next();
-
-	                    if(answer.equals("y")){
-	                        continue;
-	                    }else if(answer.equals("n")){
-	                    	System.out.println("Thanks " +  name + "for using my calculator ;)");
-	                        System.exit(0);
-	                    }else{
-	                        System.out.println("Invalid answer!!");
-	                        System.exit(0);
-	               }
-
-	           }else if(operation.equals("division")){
-	               System.out.println("Enter with the dividend: ");
-	               num1 = scan.nextDouble();
-
-	               System.out.println("Enter with the divisor: ");
-	               num2 = scan.nextDouble();
-
-	               calculation = num1 / num2;
-	               System.out.println(calculation);
-
-	               System.out.println("--> Want to do another operation? (y/n): ");
-	               answer = scan.next();
-
-	                         if(answer.equals("y")){
-	                             continue;
-	                         }else if(answer.equals("n")){
-	                        	 System.out.println("Thanks " + name + " for using my calculator ;)");
-	                             System.exit(0);
-	                         }else{
-	                             System.out.println("Invalid answer!!");
-	                             System.exit(0);
-	                         }
-
-	           }else if (operation.equals("sum")){
-	               System.out.println("Enter with the first number: ");
-	               num1 = scan.nextDouble();
-
-	               System.out.println("Enter with the second number: ");
-	               num2 = scan.nextDouble();
-
-	               calculation = num1 + num2;
-	               System.out.println(calculation);
-
-	               System.out.println("--> Want to do another operation? (y/n): ");
-	               answer = scan.next();
-
-	                         if(answer.equals("y")){
-	                             continue;
-	                         }else if(answer.equals("n")){
-	                        	 System.out.println("Thanks " + name +  " for using my calculator ;)");
-	                             System.exit(0);
-	                         }else{
-	                             System.out.println("Invalid answer!!");
-	                             System.exit(0);
-	                         }
-
-	           }else if (operation.equals("substraction")){
-	               System.out.println("Enter with the first number: ");
-	               num1 = scan.nextDouble();
-
-	               System.out.println("Enter with the second second: ");
-	               num2 = scan.nextDouble();
-
-	               calculation = num1 - num2;
-	               System.out.println(calculation);
-
-	               System.out.println("--> Want to do another operation? (y/n): ");
-	               answer = scan.next();
-
-	                         if(answer.equals("y")){
-	                             continue;
-	                         }else if(answer.equals("n")){
-	                        	 System.out.println("Thanks " + name + " for using my calculator ;)");
-	                             System.exit(0);
-	                         }else{
-	                             System.out.println("Invalid answer!!");
-	                             System.exit(0);
-	                         }
-
-	           }else {
-	               System.out.println("Invalid operation!!");
-	           }
-	        }
-	    }
+        }
+        
+    }
+    
 }
